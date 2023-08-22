7d6701fb5da7171dd23213a7f25a418a1216823d
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-08-22 23:37:18 +0500

    pre order traversal

--- BinaryTree_Implementation.java
+++ BinaryTree_Implementation.java
@@ -4,7 +4,7 @@ public class BinaryTree_Implementation {
         int data;
         
         Node left;
-        
+
         Node right;
 
         Node(int data) {
@@ -40,12 +40,31 @@ public class BinaryTree_Implementation {
         }
     }
 
+    public static void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        System.out.print(root.data+" ");
+
+        preOrder(root.left);
+        
+        preOrder(root.right);
+    
+    }
+
     public static void main(String[] args) {
+
+        System.out.println();
 
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
         Node root = BinaryTree.buildTree(nodes);
 
-        System.out.println(root.data);
+        // System.out.println(root.data);
+
+        preOrder(root);
+
+        System.out.println();
     }
 }
