f2af8b9cf2fe9adf152f6496b1b7fde9bfafad4c
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-08-22 23:23:34 +0500

    updated

--- BinaryTree_Implementation.java
+++ BinaryTree_Implementation.java
@@ -1,37 +1,51 @@
-public class BinaryTree_Implementation{
-    
-    static class Node{
+public class BinaryTree_Implementation {
+    static class Node {
+        
         int data;
+        
         Node left;
+        
         Node right;
 
-        Node(int data){
+        Node(int data) {
+            
             this.data = data;
+            
             left = null;
+            
             right = null;
         }
     }
 
-    static class BinaryTree{
+    static class BinaryTree {
+
         public static int index = -1;
-        public static Node buildTree(int nodes[]){
+
+        public static Node buildTree(int nodes[]) {
+
             index++;
-            if(nodes[index] == -1){
+
+            if (nodes[index] == -1) {
                 return null;
             }
 
+            
             Node newNode = new Node(nodes[index]);
+            
             newNode.left = buildTree(nodes);
+            
             newNode.right = buildTree(nodes);
+            
             return newNode;
         }
     }
-    
+
     public static void main(String[] args) {
-        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
         Node root = BinaryTree.buildTree(nodes);
-   
+
         System.out.println(root.data);
-}
+    }
 }
